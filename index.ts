c4588c1e58cbf96c5614023bcf6e20411b63a05d
dora.xyz <dora.xyz@icloud.com>
2023-06-17 11:50:30 +0900

    TypeScriptの復習：Linterとしての側面を利用する為とは何か？

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 let hello: string = 'Hello World';
 console.log(hello);
 
+// ドキュメントとしての側面を利用する為とは何か？（例）
 // 関数（function）関数名（add）ドキュメント
 function add(a: number, b: number): number {
     return a + b;
---
+++ person.ts
@@ -0,0 +1,3 @@
+// Linterとしての側面を利用する為とは何か？（例）
+// 例えエラーが起きてもTypeScriptはJavaScriptにコンパイルされる
+console.log(person);
