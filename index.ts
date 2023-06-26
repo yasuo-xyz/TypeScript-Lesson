36b8643f6856b271e68c11be69d67fb2404be965
dora.xyz <dora.xyz@icloud.com>
2023-06-27 07:12:31 +0900

    TypeScriptの復習：どんな型にもなるany型について知る

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 let hello: string = 'Hello World';
 console.log(hello);
 
-// ドキュメントとしての側面を利用する為とは何か？（例）
+// ドキュメントとしての側面を利用する為とは何か（例）
 // 関数（function）関数名（add）ドキュメント
 function add(a: number, b: number): number {
     return a + b;
 }
 
-// boolean型、number型、string型の例
+// boolean型、number型、string型（例）
 // boolean型はtrueかfalseのみ
 let hasValue: boolean = true;
 // number型は整数、小数、負の数、16進数、2進数、8進数がある
@@ -22,14 +22,14 @@ let single: string = 'hello';
 let double: string = "hello";
 let back: string = `hello`;
 
-// 型注釈と型推論の例
+// 型注釈と型推論（例）
 // 型注釈とは、変数に型を明示的に指定すること、英語でいうとアノテーション
 let hasValue1: boolean = true;
 // 型推論とは、型注釈を省略しても型を推論してくれる機能
 let hasValue2 = true;
 // 基本的には型推論を使い、型推論では型が推論できない場合は型注釈を使う
 
-// オブジェクトに型を付ける方法の例
+// オブジェクトに型を付ける方法（例）
 // オブジェクト型とゆうのもあるのだがその場合は全体的なオブジェクトを表してるとゆう意味になる、{}のみも同様
 const person: {
     name: string;
@@ -54,7 +54,7 @@ console.log(person2);
 // オブジェクトリテラルで型を定義する方法
 // オブジェクトリテラルで型を定義する場合は、オブジェクトの各プロパティに型を定義する
 
-// 配列に型を付けるArray型はこう使うの例
+// 配列に型を付けるArray型はこう使う（例）
 const fruits: string[] = ['Apple', 'Banana', 'Grape'];
 // Array型何でも入るって意味
 const fruits2: any[] = ['Apple', 'Banana', 'Grape', 1];
@@ -62,7 +62,7 @@ const fruits2: any[] = ['Apple', 'Banana', 'Grape', 1];
 // 絶対に1つの型しか入れられない訳ではない
 const fruits3 = ['Apple', 'Banana', 'Grape', 1];
 
-// Tuple型を使用して、決まった内容の配列を作る方法の例
+// Tuple型を使用して、決まった内容の配列を作る方法（例）
 // Tuple（読み：タプル）型は、決まった内容の配列を作る時に使う
 // 配列にも厳しく制限が掛かったものになる
 // TypeScriptは初期値は厳しくするのだがそれ以降は緩くなる
@@ -70,7 +70,7 @@ const book: [string, number, boolean] = ['business', 1500, false];
 book.push(21);
 console.log(book[3]);
 
-// Enumを使って、特定のまとまったグループのみを受け入れる列挙を使う方法の例
+// Enumを使って、特定のまとまったグループのみを受け入れる列挙を使う方法（例）
 // 列挙型とゆう新しい型を定義する時に使うEnum（イーナム）型がある
 // Enum型は、特定のまとまったグループのみを受け入れる列挙を使う時に使う
 // それを作るのにEnum型が必要になる
@@ -104,3 +104,18 @@ const coffee2 = {
     size: CoffeeSize2.TALL
 }
 coffee2.size = CoffeeSize2.SHORT;
+
+// どんな型にもなるany型について知る方法（例）
+// any型はとは、なんでも入る型
+let anything: any = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+anything.fjiafj = 'fjiafj';
+// この書き方もある
+// これは、any型を使わないで、なんでも入る型を作る方法
+let banana = 'banana';
+banana = anything;
+// もし正しくTypeScriptを書く場合は、any型はなるべく使わない方がいい
+
+// Union型を使って、複数の型を許容する方法（例）
