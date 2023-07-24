d699cb579f235358e681398a3ae6837b35d9ffa5
dora.xyz <dora.xyz@icloud.com>
2023-07-24 13:59:52 +0900

    public修飾子とprivate修飾子を使用して、アクセスを制限する方法

--- class.ts
+++ class.ts
@@ -5,31 +5,41 @@
 // クラスを定義すると、同時にインスタンスを表す今回の場合だとPerson型が生成される
 class Person {
     // nameフィールド、nameプロパティと言われる、nameの部分はどんな名前でも良い
+    // デフォルトではパブリックになる（どこでも使える）
     name: string;
+    // ageフィールド
+    // プライベートにすると、クラスの外からはアクセスできなくなる
+    private age: number;
+
     // コンストラクタ関数（予約語、メソッド）
     // 何をするものかとゆうと、今回Personとゆう設計図を元にオブジェクトを作成する時に実行される関数になる
     // つまり初期化とかする時、最初の処理をする時に便利な関数
-    constructor(initName: string) {
+    constructor(initName: string, initAge: number) {
         // thisはクラス内のフィールドを参照する時に使う
         this.name = initName;
+        this.age = initAge;
+    }
+    incrementAge() {
+        this.age += 1;
     }
     // クラスにメソッドを追加する方法（例）
     // greetingメソッド（第1引数）挨拶をするとゆう意味
     greeting(this: Person) {
-        console.log(`Hello! My name is ${this.name}`);
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
 // newキーワードを使ってPersonクラスのオブジェクトを作成する事が出来る
-const quill = new Person("Quill");
+const quill = new Person("Quill", 38);
 // console.log(quill);
+quill.incrementAge();
 quill.greeting();
 
 // オブジェクト
 // 上記のgretingメソッドをそのまま使い回してる感じ
-const anotherQuill = {
+// const anotherQuill = {
     // nameを追加している
-    name: "anotherQuill",
+    // name: "anotherQuill",
     // 下記の部分がthisになるがnameがない状態になるのでundefined（エラー）になる
-    greeting: quill.greeting
-}
-anotherQuill.greeting();
+    // greeting: quill.greeting
+// }
+// anotherQuill.greeting();
